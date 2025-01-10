99eb2983817b69fe08957bc2b9c03ba8d9fa5364
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2025-01-10 14:46:44 +0200

    daq2/kc705: Add support for the xcvr automation
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- projects/daq2/kc705/system_project.tcl
+++ projects/daq2/kc705/system_project.tcl
@@ -7,6 +7,17 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
+global FILE_PATHS
+
+set FILE_PATHS [adi_project_make xcvr_wizard [list \
+  LANE_RATE [get_env_param LANE_RATE  5] \
+  REF_CLK   [get_env_param REF_CLK    125]   \
+  PLL_TYPE  [get_env_param PLL_TYPE  CPLL]\
+]]
+puts "xcvr config path in sys proj: [dict get $FILE_PATHS cfng_file_path]"
+puts "local param file path: [dict get $FILE_PATHS param_file_path]"
+
+
 # get_env_param retrieves parameter value from the environment if exists,
 # other case use the default value
 #
