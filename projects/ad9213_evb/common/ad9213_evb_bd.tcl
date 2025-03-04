analogdevicesinc/hdl

ad9213_evb: Replace adcfifo with data_offload
Signed-off-by: Ionut Podgoreanu <ionut.podgoreanu@analog.com>

projects/ad9213_evb/common/ad9213_evb_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2024 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -12,10 +12,10 @@ set RX_SAMPLE_WIDTH 16     ; # N/NP
 set RX_SAMPLES_PER_CHANNEL 32 ; # L * 32 / (M * N)
 
 source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
+source $ad_hdl_dir/projects/common/xilinx/data_offload_bd.tcl
 
-set adc_fifo_name axi_ad9213_fifo
+set adc_offload_name ad9213_data_offload
 set adc_data_width 512
-set adc_dma_data_width 512
 
 create_bd_port -dir I glbl_clk_0
 
@@ -53,7 +53,29 @@ adi_tpl_jesd204_rx_create rx_ad9213_tpl_core $RX_NUM_OF_LANES \
                                              $RX_SAMPLES_PER_FRAME \
                                              $RX_SAMPLE_WIDTH
 
-ad_adcfifo_create $adc_fifo_name $adc_data_width $adc_dma_data_width $adc_fifo_address_width
+ad_ip_instance util_cpack2 util_ad9213_cpack [list \
+  NUM_OF_CHANNELS $RX_NUM_OF_CONVERTERS \
+  SAMPLES_PER_CHANNEL $RX_SAMPLES_PER_CHANNEL \
+  SAMPLE_DATA_WIDTH $RX_SAMPLE_WIDTH \
+]
+
+ad_data_offload_create $adc_offload_name \
+                       0 \
+                       $adc_offload_type \
+                       $adc_offload_size \
+                       $adc_data_width \
+                       $adc_data_width
+
+ad_ip_parameter $adc_offload_name/i_data_offload CONFIG.SYNC_EXT_ADD_INTERNAL_CDC 0
+ad_connect $adc_offload_name/sync_ext GND
+
+ad_ip_instance util_vector_logic rx_do_rstout_logic
+ad_ip_parameter rx_do_rstout_logic config.c_operation {not}
+ad_ip_parameter rx_do_rstout_logic config.c_size {1}
+
+ad_ip_instance util_vector_logic cpack_reset_logic
+ad_ip_parameter cpack_reset_logic config.c_operation {or}
+ad_ip_parameter cpack_reset_logic config.c_size {1}
 
 ad_ip_instance axi_dmac axi_ad9213_dma
 ad_ip_parameter axi_ad9213_dma CONFIG.DMA_TYPE_SRC 1
@@ -66,8 +88,8 @@ ad_ip_parameter axi_ad9213_dma CONFIG.DMA_LENGTH_WIDTH 24
 ad_ip_parameter axi_ad9213_dma CONFIG.DMA_2D_TRANSFER 0
 ad_ip_parameter axi_ad9213_dma CONFIG.MAX_BYTES_PER_BURST 4096
 ad_ip_parameter axi_ad9213_dma CONFIG.CYCLIC 0
-ad_ip_parameter axi_ad9213_dma CONFIG.DMA_DATA_WIDTH_SRC $adc_dma_data_width
-ad_ip_parameter axi_ad9213_dma CONFIG.DMA_DATA_WIDTH_DEST $adc_dma_data_width
+ad_ip_parameter axi_ad9213_dma CONFIG.DMA_DATA_WIDTH_SRC $adc_data_width
+ad_ip_parameter axi_ad9213_dma CONFIG.DMA_DATA_WIDTH_DEST $adc_data_width
 
 # reference clocks & resets
 
@@ -112,15 +134,19 @@ delete_bd_objs [get_bd_nets util_adc_xcvr_rx_out_clk_0]
 # connect clocks
 # device clock domain
 ad_connect  glbl_clk_0 rx_ad9213_tpl_core/link_clk
-
-ad_connect  glbl_clk_0 axi_ad9213_fifo/adc_clk
+ad_connect  glbl_clk_0 util_ad9213_cpack/clk
+ad_connect  glbl_clk_0 $adc_offload_name/s_axis_aclk
 
 # dma clock domain
-ad_connect  $sys_cpu_clk axi_ad9213_fifo/dma_clk
+ad_connect  $sys_cpu_clk $adc_offload_name/m_axis_aclk
 ad_connect  $sys_cpu_clk axi_ad9213_dma/s_axis_aclk
 
 # connect resets
-ad_connect  glbl_clk_0_rstgen/peripheral_reset axi_ad9213_fifo/adc_rst
+ad_connect  glbl_clk_0_rstgen/peripheral_reset cpack_reset_logic/op1
+ad_connect  rx_do_rstout_logic/res cpack_reset_logic/op2
+ad_connect  cpack_reset_logic/res util_ad9213_cpack/reset
+ad_connect  glbl_clk_0_rstgen/peripheral_aresetn $adc_offload_name/s_axis_aresetn
+ad_connect  $sys_cpu_resetn $adc_offload_name/m_axis_aresetn
 ad_connect  $sys_cpu_resetn axi_ad9213_dma/m_dest_axi_aresetn
 
 # connect dataflow
@@ -128,15 +154,19 @@ ad_connect  axi_ad9213_jesd/rx_sof rx_ad9213_tpl_core/link_sof
 ad_connect  axi_ad9213_jesd/rx_data_tdata rx_ad9213_tpl_core/link_data
 ad_connect  axi_ad9213_jesd/rx_data_tvalid rx_ad9213_tpl_core/link_valid
 
-ad_connect rx_ad9213_tpl_core/adc_valid_0 axi_ad9213_fifo/adc_wr
-ad_connect rx_ad9213_tpl_core/adc_data_0 axi_ad9213_fifo/adc_wdata
+ad_connect  rx_ad9213_tpl_core/adc_valid_0 util_ad9213_cpack/fifo_wr_en
+ad_connect  rx_ad9213_tpl_core/adc_enable_0 util_ad9213_cpack/enable_0
+ad_connect  rx_ad9213_tpl_core/adc_data_0 util_ad9213_cpack/fifo_wr_data_0
+ad_connect  rx_ad9213_tpl_core/adc_dovf util_ad9213_cpack/fifo_wr_overflow
 
-ad_connect rx_ad9213_tpl_core/adc_dovf axi_ad9213_fifo/adc_wovf
+ad_connect  util_ad9213_cpack/packed_fifo_wr_data $adc_offload_name/s_axis_tdata
+ad_connect  util_ad9213_cpack/packed_fifo_wr_en $adc_offload_name/s_axis_tvalid
+ad_connect  $adc_offload_name/s_axis_tlast GND
+ad_connect  $adc_offload_name/s_axis_tkeep VCC
+ad_connect  $adc_offload_name/s_axis_tready rx_do_rstout_logic/op1
 
-ad_connect  axi_ad9213_fifo/dma_wr axi_ad9213_dma/s_axis_valid
-ad_connect  axi_ad9213_fifo/dma_wdata axi_ad9213_dma/s_axis_data
-ad_connect  axi_ad9213_fifo/dma_wready axi_ad9213_dma/s_axis_ready
-ad_connect  axi_ad9213_fifo/dma_xfer_req axi_ad9213_dma/s_axis_xfer_req
+ad_connect  $adc_offload_name/m_axis axi_ad9213_dma/s_axis
+ad_connect  $adc_offload_name/init_req axi_ad9213_dma/s_axis_xfer_req
 
 ad_ip_instance axi_quad_spi hmc7044_spi
 ad_ip_parameter hmc7044_spi CONFIG.C_USE_STARTUP 0
@@ -159,6 +189,7 @@ ad_cpu_interconnect 0x44a10000 rx_ad9213_tpl_core
 ad_cpu_interconnect 0x44a90000 axi_ad9213_jesd
 ad_cpu_interconnect 0x44a71000 hmc7044_spi
 ad_cpu_interconnect 0x7c420000 axi_ad9213_dma
+ad_cpu_interconnect 0x7c430000 $adc_offload_name
 
 # interconnect (gt/adc)
 ad_mem_hp0_interconnect $sys_cpu_clk axi_ad9213_xcvr/m_axi

philippdiethelm/adi-hdl

projects/ad9213_evb/common/ad9213_evb_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2022-2023 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2022-2025 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -12,8 +12,9 @@ set RX_SAMPLE_WIDTH 16     ; # N/NP
 set RX_SAMPLES_PER_CHANNEL 32 ; # L * 32 / (M * N)
 
 source $ad_hdl_dir/library/jesd204/scripts/jesd204.tcl
+source $ad_hdl_dir/projects/common/xilinx/data_offload_bd.tcl
 
-set adc_fifo_name axi_ad9213_fifo
+set adc_offload_name ad9213_data_offload
 set adc_data_width 512
 set adc_dma_data_width 512
 
@@ -53,7 +54,15 @@ adi_tpl_jesd204_rx_create rx_ad9213_tpl_core $RX_NUM_OF_LANES \
                                              $RX_SAMPLES_PER_FRAME \
                                              $RX_SAMPLE_WIDTH
 
-ad_adcfifo_create $adc_fifo_name $adc_data_width $adc_dma_data_width $adc_fifo_address_width
+ad_data_offload_create $adc_offload_name \
+                       0 \
+                       $adc_offload_type \
+                       $adc_offload_size \
+                       $adc_data_width \
+                       $adc_dma_data_width
+
+ad_ip_parameter $adc_offload_name/i_data_offload CONFIG.SYNC_EXT_ADD_INTERNAL_CDC 0
+ad_connect $adc_offload_name/sync_ext GND
 
 ad_ip_instance axi_dmac axi_ad9213_dma
 ad_ip_parameter axi_ad9213_dma CONFIG.DMA_TYPE_SRC 1
@@ -112,15 +121,15 @@ delete_bd_objs [get_bd_nets util_adc_xcvr_rx_out_clk_0]
 # connect clocks
 # device clock domain
 ad_connect  glbl_clk_0 rx_ad9213_tpl_core/link_clk
-
-ad_connect  glbl_clk_0 axi_ad9213_fifo/adc_clk
+ad_connect  glbl_clk_0 $adc_offload_name/s_axis_aclk
 
 # dma clock domain
-ad_connect  $sys_cpu_clk axi_ad9213_fifo/dma_clk
+ad_connect  $sys_cpu_clk $adc_offload_name/m_axis_aclk
 ad_connect  $sys_cpu_clk axi_ad9213_dma/s_axis_aclk
 
 # connect resets
-ad_connect  glbl_clk_0_rstgen/peripheral_reset axi_ad9213_fifo/adc_rst
+ad_connect  glbl_clk_0_rstgen/peripheral_aresetn $adc_offload_name/s_axis_aresetn
+ad_connect  $sys_cpu_resetn $adc_offload_name/m_axis_aresetn
 ad_connect  $sys_cpu_resetn axi_ad9213_dma/m_dest_axi_aresetn
 
 # connect dataflow
@@ -128,15 +137,15 @@ ad_connect  axi_ad9213_jesd/rx_sof rx_ad9213_tpl_core/link_sof
 ad_connect  axi_ad9213_jesd/rx_data_tdata rx_ad9213_tpl_core/link_data
 ad_connect  axi_ad9213_jesd/rx_data_tvalid rx_ad9213_tpl_core/link_valid
 
-ad_connect rx_ad9213_tpl_core/adc_valid_0 axi_ad9213_fifo/adc_wr
-ad_connect rx_ad9213_tpl_core/adc_data_0 axi_ad9213_fifo/adc_wdata
+ad_connect  rx_ad9213_tpl_core/adc_valid_0 $adc_offload_name/s_axis_tvalid
+ad_connect  rx_ad9213_tpl_core/adc_data_0 $adc_offload_name/s_axis_tdata
+ad_connect  rx_ad9213_tpl_core/adc_dovf GND
 
-ad_connect rx_ad9213_tpl_core/adc_dovf axi_ad9213_fifo/adc_wovf
+ad_connect  $adc_offload_name/s_axis_tlast GND
+ad_connect  $adc_offload_name/s_axis_tkeep VCC
 
-ad_connect  axi_ad9213_fifo/dma_wr axi_ad9213_dma/s_axis_valid
-ad_connect  axi_ad9213_fifo/dma_wdata axi_ad9213_dma/s_axis_data
-ad_connect  axi_ad9213_fifo/dma_wready axi_ad9213_dma/s_axis_ready
-ad_connect  axi_ad9213_fifo/dma_xfer_req axi_ad9213_dma/s_axis_xfer_req
+ad_connect  $adc_offload_name/m_axis axi_ad9213_dma/s_axis
+ad_connect  $adc_offload_name/init_req axi_ad9213_dma/s_axis_xfer_req
 
 ad_ip_instance axi_quad_spi hmc7044_spi
 ad_ip_parameter hmc7044_spi CONFIG.C_USE_STARTUP 0
@@ -159,6 +168,7 @@ ad_cpu_interconnect 0x44a10000 rx_ad9213_tpl_core
 ad_cpu_interconnect 0x44a90000 axi_ad9213_jesd
 ad_cpu_interconnect 0x44a71000 hmc7044_spi
 ad_cpu_interconnect 0x7c420000 axi_ad9213_dma
+ad_cpu_interconnect 0x7c430000 $adc_offload_name
 
 # interconnect (gt/adc)
 ad_mem_hp0_interconnect $sys_cpu_clk axi_ad9213_xcvr/m_axi